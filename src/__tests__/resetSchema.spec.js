cf1f6e1befb044030efcb1828c96fefa4f0a98f0
Alexander Vetter <vettera@gmail.com>
2017-11-21 14:10:25 +0100

    remove resetSchema test

--- src/__tests__/resetSchema.spec.js
+++
@@ -1,9 +0,0 @@
-const structorizer = require("../index")("http://localhost:8181");
-
-describe("resetSchema", () => {
-  it("should call backend with nonce", () => {
-    const result = structorizer.api.resetSchema("nonce");
-
-    return expect(result).toEqual({"status": "ok"});
-  });
-});
